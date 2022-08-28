3dbe99261d60cc2d466bebee9d6d829bf1d19672
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-08-27 22:40:46 -0600

    added question inputs

--- Develop/index.js
+++ Develop/index.js
@@ -1,76 +1,72 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
-const generateHTML = ({ name, location, github, linkedin }) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${name}</h1>
-    <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
-
+const generateReadMe = ({
+  title,
+  description,
+  TOC,
+  installation,
+  usage,
+  license,
+  tests,
+  contributors,
+  github,
+}) =>
+  `This is an auto generated readme file. My name is ${name}. I speak ${languages}. I prefer to be contaced in ${preferred} language`;
 inquirer
   .prompt([
     {
       type: "input",
-      name: "name",
-      message: "What is your name?",
+      message: "What is your project title?",
+      name: "title",
     },
     {
       type: "input",
-      name: "location",
-      message: "Where are you from?",
+      message: "Description of project: ",
+      name: "description",
     },
     {
       type: "input",
+      message: "Table of Contents: ",
+      name: "TOC",
+    },
+    {
+      type: "input",
+      message: "Describe installation instructions: ",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Describe usage information: ",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "What license is used?",
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "List Contributors: ",
+      name: "contributors",
+    },
+    {
+      type: "input",
+      message: "Test instructions: ",
+      name: "tests",
+    },
+
+    {
+      type: "input",
+      message: "What is your github username?",
       name: "github",
-      message: "Enter your GitHub Username",
-    },
-    {
-      type: "input",
-      name: "linkedin",
-      message: "Enter your LinkedIn URL.",
     },
   ])
+
   .then((answers) => {
     console.log(answers);
-    const htmlPageContent = generateHTML(answers);
+    const readMePageContent = generateReadMe(answers);
 
-    fs.writeFile("index.html", htmlPageContent, (err) =>
-      err ? console.log(err) : console.log("Successfully created index.html!")
+    fs.writeFile("README1.md", readMePageContent, (err) =>
+      err ? console.log(err) : console.log("Successfully created readme1.md!")
     );
   });
-
-//   const fs = require("fs");
-
-// fs.appendFile("log.txt", `${process.argv[2]}\n`, (err) =>
-//   err ? console.error(err) : console.log("Commit logged!")
-// );
